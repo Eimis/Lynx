b665a5b76b4ce07a05e85909ab2e0a57de965831
Eimantas <eimantas.stonys@gmail.com>
2014-04-19 17:07:30 +0300

    settings

--- lynx/settings.py
+++ lynx/settings.py
@@ -35,7 +35,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['lynxapp.io']
+ALLOWED_HOSTS = ['lynxapp.io', 'localhost', '127.0.0.1']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -75,7 +75,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = 'lynx_static_files/static'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
