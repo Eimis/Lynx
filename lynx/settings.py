cfeacf11b5539e6167941fc96e9b1b608df8ed51
Eimantas <eimantas.stonys@gmail.com>
2014-04-19 16:08:52 +0300

    hosts

--- lynx/settings.py
+++ lynx/settings.py
@@ -35,7 +35,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['lynxapp.io']
+ALLOWED_HOSTS = ['*']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
