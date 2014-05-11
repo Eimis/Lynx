3e1dce7d99853f409fb2a0aec672fe356b2ab874
Eimantas <eimantas.stonys@gmail.com>
2014-05-11 10:18:31 +0300

    README.md

--- lynx/test.py
+++ lynx/test.py
@@ -1,18 +1,18 @@
-def Hello(request):
-	''' Register '''
-	form = RegistrationForm()
-	if request.method == 'POST':
-		form = RegistrationForm(request.POST)
-		if '@' not in request.POST['username']: # TODO already exists
-			email_not_valid = "This is not a valid email address"
-			return render(request, "hello.html",{"form" : form, "email_not_valid" : email_not_valid,})
-		elif form.is_valid():
-			new_user = User.objects.create_user(username=request.POST['username'],password=request.POST['password'])
-			return HttpResponseRedirect("/dashboard/")
-		return render(request, "hello.html",{"form" : form})
-	else: # request 'GET'
-		return render(request, "hello.html",{"form" : form})
 
-@login_required(redirect_field_name=None) # get rid of /next/ stuff in url when not logged in
-def Dashboard(request):
-	return render(request, "dashboard.html",)
+''' The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ? '''
+
+
+
+def tpf(n):
+	x = 1
+	sar = []
+	while x * x < n:
+		while n % x == 0:
+			n = n / x
+			sar.append(n)
+		x+=1
+	print(max(sar))
+
+tpf(600851475143)
