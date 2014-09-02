85766253346ed9ff07960b31f860e5cf930e53b3
Eimantas <eimantas.stonys@gmail.com>
2014-09-02 22:11:02 +0300

    removed settings

--- lynx/dev_settings.py
+++ lynx/dev_settings.py
@@ -4,7 +4,7 @@ DATABASES = {
         'NAME': 'lynxdb',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'eimantas',
-        'PASSWORD': 'maxmaxkit',
+        'PASSWORD': '',
         'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
